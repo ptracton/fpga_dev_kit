e52a699f3c72b4b172e42ad64c69f7bca2d16a1f
Phil Tracton <ptracton@gmail.com>
2012-04-02 20:53:28 -0700

    Initial release

---
+++ tools/fpga.sh
@@ -0,0 +1,6 @@
+
+
+export XILINX=/opt/Xilinx/12.2/ISE_DS/ISE
+export MODELSIM=/opt/altera/11.1/modelsim_ase
+export ALTERA=/opt/altera/11.1/quartus
+export FPGA_CORES=/home/ptracton/src/hardware/fpga_cores
