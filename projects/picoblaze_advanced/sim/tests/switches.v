b9274f5c4c7d808711afafad000d726239ed63ab
Phil Tracton <ptracton@gmail.com>
2012-05-04 18:54:42 -0500

    Updated test cases

--- projects/picoblaze_advanced/sim/tests/switches.v
+++ projects/picoblaze_advanced/sim/tests/switches.v
@@ -5,10 +5,11 @@ initial
 
      @(posedge `DUT.register_file.sim_status[1]);
      testbench.switches_tb <= 8'h01;
-     $display("Assert Switch %d @ %d", testbench.switches_tb, $time);     
-     @(negedge `DUT.register_file.sim_status[1]);
-     testbench.switches_tb <= 8'h00;
-     $display("De-assert Switch %d @ %d", testbench.switches_tb, $time);     
+     $display("Assert Switch %d @ %d", testbench.switches_tb, $time);
+     repeat(5) @(posedge testbench.clk_tb);
+     testbench.switches_tb <= 8'h00;     
+     
+     
      
      #10 $display("THREAD DONE @ %d", $time);     
      testbench.thread_pass <= 1'b1;      
